f4c6c112a4f73192ad791315ea606fb7a559d781
Veronica-rhody-219 <ngocdiep219@gmail.com>
2023-03-19 10:53:04 +0700

    feat: update computerImg src

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
+import computerImg from "../public/assets/computer.jpg";
 
 const About = () => {
   return (
@@ -14,7 +15,8 @@ const About = () => {
           <p className="py-4 text-gray-600 ">
             Hello, my name is Veronica. Language is always my interest. I can
             speak Vietnamese, English, Chinese, and Russian. One day I realized
-            that I wanted to create something useful with languages, then I started my journey to learn programming languages.
+            that I wanted to create something useful with languages, then I
+            started my journey to learn programming languages.
           </p>
 
           <p className="py-2 text-gray-600 ">
@@ -26,16 +28,16 @@ const About = () => {
             create something from nothing.
           </p>
           <p className="py-2 text-gray-600 ">
-            From the very beginning, I always wanted to learn machine
-            learning because I know it would have a very promising future but it
-            was too difficult for an absolute beginner like me, that’s why I
-            took the JavaScript course, and study more about its frameworks like
-            React and Angular. JavaScript is a beautiful language, the more I
-            study the more I fall in love with it and now I know I can study
-            machine learning with tensorflowjs which is written in JavaScript
-            and that’s wonderful. After finishing this course I definitely study
-            machine learning with tensorflowjs and then I plan to study Python
-            which is a common language for machine learning.
+            From the very beginning, I always wanted to learn machine learning
+            because I know it would have a very promising future but it was too
+            difficult for an absolute beginner like me, that’s why I took the
+            JavaScript course, and study more about its frameworks like React
+            and Angular. JavaScript is a beautiful language, the more I study
+            the more I fall in love with it and now I know I can study machine
+            learning with tensorflowjs which is written in JavaScript and that’s
+            wonderful. After finishing this course I definitely study machine
+            learning with tensorflowjs and then I plan to study Python which is
+            a common language for machine learning.
           </p>
           <p className="py-2 text-gray-600 underline cursor-pointer">
             <Link href="/#project">Check out some of my projects</Link>
@@ -44,7 +46,7 @@ const About = () => {
         <div className="w-full h-auto shadow-xl shadow-gray-400 rounded-xl flex justify-center items-center p-4 hover:scale-105 ease-in duration-300">
           <Image
             className="rounded-xl"
-            src="/../public/assets/computer.jpg"
+            src={computerImg}
             width="640"
             height="426"
             alt="image1"
